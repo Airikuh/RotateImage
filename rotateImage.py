7e80c15e3bd0cb8d5e5fb06e0af87933e6b36c9d
Erika Valle-Baird <68307227+Airikuh@users.noreply.github.com>
2023-10-11 06:15:31 -0400

    Create rotateImage.py

---
+++ rotateImage.py
@@ -0,0 +1,208 @@
+#TASK 4: Use texture (AI.jpg) and rotate about x, y, z axes, simultaneously
+#PYRR to Define various transformation matrices
+#Pyrr used to specify perspective projection matrix and transformation matrix
+import pygame as pg
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram,compileShader
+import numpy as np
+import pyrr
+
+
+class Cube:
+    def __init__(self, position, eulers):
+        self.position = np.array(position, dtype=np.float32)
+        self.eulers = np.array(eulers, dtype=np.float32)
+
+class App:
+    def __init__(self):
+        #initialise pygame
+        pg.init()
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
+                                    pg.GL_CONTEXT_PROFILE_CORE)
+        pg.display.set_mode((640,480), pg.OPENGL|pg.DOUBLEBUF)
+        self.clock = pg.time.Clock()
+        self.cube_mesh = CubeMesh()
+#initialise opengl
+        glClearColor(0.1, 0.2, 0.2, 1)   
+        self.shader = self.createShader("Shader/vertex2.txt", "Shader/fragment2.txt")
+        glUseProgram(self.shader)
+        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
+        glEnable(GL_DEPTH_TEST)
+        self.my_texture = Material("Texture/AI.jpg")
+#Cube Position
+        self.cube = Cube(
+            position = [0,0,-3],
+            eulers = [0,0,0]
+          
+        )
+#CREATE PROJECTION, CODE SHOULD BE SIMILAR WITH ROTATION
+        projection_transform = pyrr.matrix44.create_perspective_projection(
+            fovy = 45, aspect = 640/480, 
+            near = 0.1, far = 10, dtype=np.float32
+        )
+        glUniformMatrix4fv(
+            glGetUniformLocation(self.shader,"projection"),
+            1, GL_FALSE, projection_transform
+        )
+        self.modelMatrixLocation = glGetUniformLocation(self.shader,"model")
+        self.mainLoop()
+
+    def createShader(self, vertexFilepath, fragmentFilepath):
+        with open(vertexFilepath,'r') as f:
+            vertex_src = f.readlines()
+        with open(fragmentFilepath,'r') as f:
+            fragment_src = f.readlines()       
+        shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER),
+                                compileShader(fragment_src, GL_FRAGMENT_SHADER))       
+        return shader
+
+    def mainLoop(self):
+        running = True
+        while (running):
+            #check events
+            for event in pg.event.get():
+                if (event.type == pg.QUIT):
+                    running = False            
+            #update cube
+            #self.cube.eulers[2] = (self.cube.eulers[2]+0.25)%360           
+            #yaw: y-axis, shows eulers[2]         
+            #Try x-axis: euler[1]
+            #THIS WORKED!!!!!!!!!! Setting both to 1!
+            #Causes simultaneous rotation!!
+            self.cube.eulers[1] = (self.cube.eulers[1]+0.25)%360    
+            self.cube.eulers[2] = (self.cube.eulers[2]+0.25)%360               
+            self.cube.eulers[0] = (self.cube.eulers[0]+0.25)%360   
+            #refresh screen
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            glUseProgram(self.shader)
+            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
+            """ pitch: rotation around x axis
+                roll:rotation around z axis
+                yaw: rotation around y axis
+            """
+
+#pitch: rotation around x axis
+
+            model_transform = pyrr.matrix44.multiply(
+                m2=model_transform, 
+                m1=pyrr.matrix44.create_from_translation(      # position 1
+                    vec=np.array(self.cube.position),dtype=np.float32
+                )
+            )           
+            model_transform = pyrr.matrix44.multiply(
+                m2=model_transform, 
+                m1=pyrr.matrix44.create_from_eulers(          # position 2 
+                    eulers=np.radians(self.cube.eulers), dtype=np.float32
+                )
+            )
+
+
+            #rot_x = pyrr.matrix44.create_from_x_rotation(0.5 * pygame.time.get_ticks() / 1000, dtype=numpy.float32)
+            #rot_y = pyrr.matrix44.create_from_y_rotation(0.8 * pygame.time.get_ticks() / 1000, dtype=numpy.float32)
+
+            glUniformMatrix4fv(self.modelMatrixLocation,1,GL_FALSE,model_transform)
+            self.my_texture.use()
+            glBindVertexArray(self.cube_mesh.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
+            pg.display.flip()
+            #timing
+            self.clock.tick(60)
+        self.quit()
+
+def quit(self):
+        self.cube_mesh.destroy()
+        self.my_texture.destroy()
+        glDeleteProgram(self.shader)
+        pg.quit()
+
+
+#defines a cube with its three coordinates (x, y, z) and two texture coordinates 
+class CubeMesh:
+    def __init__(self):
+        # x, y, z, s, t
+        self.vertices = (
+                -0.5, -0.5, -0.5, 0, 0,
+                 0.5, -0.5, -0.5, 1, 0,
+                 0.5,  0.5, -0.5, 1, 1,
+                 0.5,  0.5, -0.5, 1, 1,
+                -0.5,  0.5, -0.5, 0, 1,
+                -0.5, -0.5, -0.5, 0, 0,
+                -0.5, -0.5,  0.5, 0, 0,
+                 0.5, -0.5,  0.5, 1, 0,
+                 0.5,  0.5,  0.5, 1, 1,
+                 0.5,  0.5,  0.5, 1, 1,
+                -0.5,  0.5,  0.5, 0, 1,
+                -0.5, -0.5,  0.5, 0, 0,
+                -0.5,  0.5,  0.5, 1, 0,
+                -0.5,  0.5, -0.5, 1, 1,
+                -0.5, -0.5, -0.5, 0, 1,
+                -0.5, -0.5, -0.5, 0, 1,
+                -0.5, -0.5,  0.5, 0, 0,
+                -0.5,  0.5,  0.5, 1, 0,
+                 0.5,  0.5,  0.5, 1, 0,
+                 0.5,  0.5, -0.5, 1, 1,
+                 0.5, -0.5, -0.5, 0, 1,
+                 0.5, -0.5, -0.5, 0, 1,
+                 0.5, -0.5,  0.5, 0, 0,
+                 0.5,  0.5,  0.5, 1, 0,
+                -0.5, -0.5, -0.5, 0, 1,
+                 0.5, -0.5, -0.5, 1, 1,
+                 0.5, -0.5,  0.5, 1, 0,
+                 0.5, -0.5,  0.5, 1, 0,
+                -0.5, -0.5,  0.5, 0, 0,
+                -0.5, -0.5, -0.5, 0, 1,
+                -0.5,  0.5, -0.5, 0, 1,
+                 0.5,  0.5, -0.5, 1, 1,
+                 0.5,  0.5,  0.5, 1, 0,
+                 0.5,  0.5,  0.5, 1, 0,
+                -0.5,  0.5,  0.5, 0, 0,
+                -0.5,  0.5, -0.5, 0, 1
+            )       
+        self.vertex_count = len(self.vertices)//5
+        self.vertices = np.array(self.vertices, dtype=np.float32)
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
+    
+    def destroy(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1,(self.vbo,))
+
+class Material:   
+    def __init__(self, filepath):
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        # convert() is a method applied to the loaded image to convert its pixel format.
+        # load() returns a pygame.Surface object that represents the loaded image. 
+        image = pg.image.load(filepath).convert() 
+        image_width,image_height = image.get_rect().size
+        img_data = pg.image.tostring(image,'RGBA')
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,image_width,image_height,0,GL_RGBA,GL_UNSIGNED_BYTE,img_data)
+        glGenerateMipmap(GL_TEXTURE_2D)
+
+    def use(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D,self.texture)
+
+    def destroy(self):
+        glDeleteTextures(1, (self.texture,))
+
+if __name__ == "__main__":
+    myApp = App()
+
+
+
+
+
